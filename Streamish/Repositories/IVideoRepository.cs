35107b444c18c1756871c3316b0af8f110d83295
Jacob Thomas <jacgretho@gmail.com>
2023-01-19 11:48:40 -0600

    added end point for searching for videos made after a certain date

--- Streamish/Repositories/IVideoRepository.cs
+++ Streamish/Repositories/IVideoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Streamish.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Streamish.Repositories
@@ -13,6 +14,7 @@ namespace Streamish.Repositories
         Video GetByIdWithComments(int id);
         void Update(Video video);
         List<Video> Search(string criterion, bool sortDescending);
+        List<Video> GetAfterDate(DateTime since);
 
     }
 }
