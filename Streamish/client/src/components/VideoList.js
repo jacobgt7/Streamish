112a2dd8bc4c9d25b4ec5d1ebe2898a7e2cfcd6b
Jacob Thomas <jacgretho@gmail.com>
2023-01-20 09:30:14 -0600

    added a search feature

--- Streamish/client/src/components/VideoList.js
+++ Streamish/client/src/components/VideoList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Video from './Video';
-import { getAllVideos, getAllVideosWithComments } from "../modules/videoManager";
+import { getAllVideos, getAllVideosWithComments, searchVideos } from "../modules/videoManager";
+import { FormGroup, Form, Input, Button } from "reactstrap";
 
 const VideoList = () => {
     const [videos, setVideos] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     // const getVideos = () => {
     //     getAllVideos().then(videos => setVideos(videos));
@@ -17,8 +19,24 @@ const VideoList = () => {
         getVideosWithComments();
     }, []);
 
+    const handleSearchButton = (event) => {
+        event.preventDefault();
+
+        searchVideos(searchText).then(videos => setVideos(videos));
+    }
+
+    const handleSearchInput = (event) => {
+        setSearchText(event.target.value)
+    }
+
     return (
         <div className="container">
+            <Form>
+                <FormGroup>
+                    <Input onChange={handleSearchInput} type="text" name="search" id="search" />
+                    <Button onClick={handleSearchButton}>Search</Button>
+                </FormGroup>
+            </Form>
             <div className="row justify-content-center">
                 {videos.map((video) => (
                     <Video video={video} key={video.id} />
